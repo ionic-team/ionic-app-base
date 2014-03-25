7446889cbbddcee617f61791430c90ec1be82adc
Max Lynch <max@drifty.com>
2014-03-24 22:30:33 -0500

    Added bower init

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var child_process = require('child_process');
 var sass = require('gulp-sass');
+var Q = require('q');
 
 var paths = {
   sass: ['www/lib/sass/**/*.scss']
@@ -15,6 +16,21 @@ gulp.task('sass', function() {
 gulp.task('watch', function() {
   gulp.watch(paths.docs, ['docs']);
   gulp.watch(paths.sass, ['sass']);
+});
+
+// Initialize the project to the most recent release
+gulp.task('init', function() {
+  var q = Q.defer();
+  console.log('Installing latest stable release of Ionic from bower');
+
+  var ls = child_process.spawn('bower', ['install', 'driftyco/ionic-bower'], {
+    stdio: 'inherit'
+  });
+  ls.on('close', function(code) {
+    q.resolve();
+  });
+
+  return Q.all([q.promise]);
 });
 
 gulp.task('default', ['sass']);
