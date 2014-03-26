c7ebc17837c8c3585134e29faf8bfc2bf51f2ed5
Andy Joslin <andytjoslin@gmail.com>
2014-03-26 10:22:34 -0600

    update: less deps, use programmatic bower for windows fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,9 @@
+var bower = require('bower');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var child_process = require('child_process');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
-var prompt = require('prompt');
-var argv = require('yargs').argv;
-var fs = require('fs');
-var ncp = require('ncp').ncp;
-var Q = require('q');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -30,20 +25,9 @@ gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
 });
 
-// Bootstrap the project by loading ionic from bower and copying over
-// Any sass files (requires the --sass flag to gulp bootstrap as it's potentially destructive).
 gulp.task('init', function() {
-  var bowerQ = Q.defer();
   console.log('Installing latest stable release of Ionic from bower');
-
-  var ls = child_process.spawn('bower', ['install'], {
-    stdio: 'inherit'
-  });
-  ls.on('close', function(code) {
-    bowerQ.resolve();
-  });
-
-  return Q.all([bowerQ.promise]);
+  return bower.commands.install();
 });
 
 gulp.task('default', ['sass']);
