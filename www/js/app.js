99635da5a15ef2832151f95af624fd4441cd3b8b
Max Lynch <max@drifty.com>
2014-05-14 10:07:45 -0500

    Remove keyboard bar by default

--- www/js/app.js
+++ www/js/app.js
@@ -7,6 +7,11 @@ angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
     if(window.StatusBar) {
       // Set the statusbar to use the default style, tweak this to
       // remove the status bar on iOS or change it to use white instead of dark colors.
