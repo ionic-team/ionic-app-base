345228357f8c865f9be8a16538cdf3d12394ee59
Max Lynch <max@drifty.com>
2015-01-15 08:30:06 -0600

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -8,7 +8,11 @@ angular.module('starter', ['ionic'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
+    // for form inputs).
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at 
+    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+    // useful especially with forms, though we would prefer giving the user a little more room
+    // to interact with the app.
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
