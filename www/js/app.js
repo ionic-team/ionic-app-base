2e11240fd7ee973bfe14a0c6a80231dfdbc957ee
mhartington <mikehartington@gmail.com>
2015-07-14 11:46:33 -0400

    disableScroll enabled by default

--- www/js/app.js
+++ www/js/app.js
@@ -9,14 +9,15 @@ angular.module('starter', ['ionic'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at 
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
     // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
     // useful especially with forms, though we would prefer giving the user a little more room
     // to interact with the app.
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       // Set the statusbar to use the default style, tweak this to
       // remove the status bar on iOS or change it to use white instead of dark colors.
       StatusBar.styleDefault();
