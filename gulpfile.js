989c562eea6304a5c8417e3ae1b7474965de78db
Ionitron <hi@ionicframework.com>
2015-10-14 09:52:09 -0400

    update sass error

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,8 @@ gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
-    .pipe(sass({
-      errLogToConsole: true
-    }))
+    .pipe(sass())
+    .on('error', sass.logError)
     .pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
